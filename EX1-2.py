6aff00cae576b69614e84f05d025ab414acffac3
MMS720 <115730905+MMS720@users.noreply.github.com>
2022-10-24 19:55:49 +0330

    Create EX1-2.py

---
+++ EX1-2.py
@@ -0,0 +1,13 @@
+def area4triangle (h,a):
+    '''
+    (int,int)=>int
+    return area of triangle whit h and a.
+    example:
+    area4triangle (3,2):
+    3
+    '''
+     if (h>0 and a>0):
+            Area=h*a/2
+     else: print("triangle is not currect")
+     return(Area)
+     
